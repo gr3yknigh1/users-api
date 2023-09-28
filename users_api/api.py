a1fc86870b76c2db6025811bd1fca42075ccb0b7
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-09-28 22:30:24 +0300

    Fix missing docs and remove subapis

--- users_api/api.py
+++ users_api/api.py
@@ -2,9 +2,14 @@ from __future__ import annotations
 
 import fastapi
 
-api_router = fastapi.APIRouter(prefix="/api/v1/users")
+api_router = fastapi.APIRouter(prefix="/api/v1")
 
 
-@api_router.get("")
-async def users_index():
+@api_router.get("/users")
+async def get_users_list():
     return {"Hello": "World"}
+
+
+@api_router.get("/health")
+async def check_server_health():
+    return "OK"
